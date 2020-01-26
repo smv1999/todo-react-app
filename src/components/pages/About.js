ab22287a4f57310610c9eb50ff7595562b1a88c5
programmersgateway <vaidhyanathansm99@gmail.com>
2020-01-26 12:13:34 +0530

    added files

---
+++ src/components/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function About() {
+    return (
+        <React.Fragment>
+            <h1>
+                About
+            </h1>
+            <p>This is a simple Todo Application.</p>
+        </React.Fragment>
+    )
+}
